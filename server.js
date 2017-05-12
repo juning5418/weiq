58bd73d0c23f4395ce40efe841dac3ff659387a1
juning <691092674@qq.com>
2017-05-12 16:42:32 +0800

    add

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var io = require('socket.io'); // 加入 Socket.IO
+var io = require('./db.js'); // 加入 Socket.IO
+
+
 
 var server = http.createServer(function (request, response) {
 
@@ -25,7 +28,19 @@ var server = http.createServer(function (request, response) {
 		        }
 		        response.end();
 		      });
-      	break;
+		      break;
+		case '/send.html':
+		      fs.readFile(__dirname +"/view"+ path, function(error, data) {
+		        if (error){
+		          response.writeHead(404);
+		          response.write("opps this doesn't exist - 404");
+		        } else {
+		          response.writeHead(200, {"Content-Type": "text/html"});
+		          response.write(data, "utf8");
+		        }
+		        response.end();
+		      });
+      		break;
     	default:
 	      response.writeHead(404);
 	      response.write("opps this doesn't exist - 404");
@@ -48,7 +63,8 @@ serv_io.sockets.on('connection', function(socket) {
 
   // 接收來自於瀏覽器的資料
   socket.on('client_data', function(data) {
-    process.stdout.write(data.letter);
+  	    socket.emit('date2', {'date': data.letter});
+		process.stdout.write(data.letter);
   });
 
 });
